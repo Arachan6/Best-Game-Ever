83a81269c8184e9c499536716a4649b436d62588
sulai306 <105258482+sulai306@users.noreply.github.com>
2022-05-13 00:18:07 -0300

    Patch 0.042 Changelog
    -Arregladas las fisicas del ThrowRock.

--- My project (1)/Assets/Scripts/Player/RockMovement.cs
+++ My project (1)/Assets/Scripts/Player/RockMovement.cs
@@ -7,10 +7,26 @@ public class RockMovement : MonoBehaviour
     public GameObject player;
     public float rockSpeed;
     private Rigidbody2D rb;
+    private bool FacingRight = true;
+    private Move PlayerMoveAtributes;
     void Start()
     {
+
+        player = GameObject.Find("Player");
+        PlayerMoveAtributes = player.GetComponent<Move>();
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.right * rockSpeed;
+
+        if (PlayerMoveAtributes.FacingRight)
+        {
+            print("Facing right");
+            rb.velocity = new Vector2(3, 0.8f);
+        }
+        if(!PlayerMoveAtributes.FacingRight)
+        {
+            print("Facing left");
+            rb.velocity = new Vector2(-3, 0.8f);
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D hitInfo)
