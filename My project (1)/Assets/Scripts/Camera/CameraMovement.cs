ce79bd072d61217ebc60335be9fe1dfcb1cec8b9
Arachan6 <fsaravia.avila@gmail.com>
2022-05-14 18:30:27 -0300

    Agregado sprites de slime

---
+++ My project (1)/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float smoothFactor = 10;
+
+    void Update()
+    {
+        Follow();
+    }
+
+    void Follow() {
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = targetPosition;
+    }
+}
